01997ed2231ed8f02711f5b49a792818e520d334
Igor Pletnev <gb1001p@yandex.ru>
2026-06-22 00:29:55 +0300

    final_version_1

---
+++ backend/recipes/management/commands/create_tags.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from recipes.models import Tag
+
+
+TAGS = [
+    {'name': 'Завтрак', 'slug': 'breakfast', 'color': '#E26C2D'},
+    {'name': 'Обед', 'slug': 'lunch', 'color': '#49B64E'},
+    {'name': 'Ужин', 'slug': 'dinner', 'color': '#8775D2'},
+]
+
+
+class Command(BaseCommand):
+    help = 'Создаёт теги по умолчанию, если их нет'
+
+    def handle(self, *args, **options):
+        if Tag.objects.exists():
+            self.stdout.write('Tags already exist')
+            return
+
+        for tag_data in TAGS:
+            Tag(**tag_data).save()
+            self.stdout.write(f'Tag created: {tag_data["name"]}')
+
+        self.stdout.write(self.style.SUCCESS(f'{len(TAGS)} tags created'))
